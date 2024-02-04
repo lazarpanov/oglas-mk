99f6731a0a4025c4b7c89630c1992e99d179edef
lazarpanov <116360184+lazarpanov@users.noreply.github.com>
2024-02-04 13:43:40 +0100

    app-layout and dark mode

---
+++ oglas-mk/src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import '../app.pcss';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header"
+		><AppBar
+			gridColumns="grid-cols-3"
+			slotDefault="place-self-center"
+			slotTrail="place-content-end"
+		>
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			<p class="text-2xl font-bold text-primary-600">Oglasium</p>
+			<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+		</AppBar></svelte:fragment
+	>
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<!-- (sidebarRight) -->
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
