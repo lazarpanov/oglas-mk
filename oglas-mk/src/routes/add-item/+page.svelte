e63178810283d40d7b5f3d4c4d11dd690ba73c1a
lazarpanov <lazarpanov2002@gmail.com>
2024-02-15 14:49:44 +0100

    create item fix

--- oglas-mk/src/routes/add-item/+page.svelte
+++ oglas-mk/src/routes/add-item/+page.svelte
@@ -13,16 +13,21 @@
 				<input class="input px-2 py-1" title="title" type="text" placeholder="Title" />
 			</label>
 			<label class="label">
-				<span>Amount</span>
+				<span>Price</span>
 				<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
 					<div class="input-group-shim">
-						<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" class="fill-white"
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							height="24"
+							viewBox="0 -960 960 960"
+							width="24"
+							class="fill-white"
 							><path
 								d="M441-120v-86q-53-12-91.5-46T293-348l74-30q15 48 44.5 73t77.5 25q41 0 69.5-18.5T587-356q0-35-22-55.5T463-458q-86-27-118-64.5T313-614q0-65 42-101t86-41v-84h80v84q50 8 82.5 36.5T651-650l-74 32q-12-32-34-48t-60-16q-44 0-67 19.5T393-614q0 33 30 52t104 40q69 20 104.5 63.5T667-358q0 71-42 108t-104 46v84h-80Z"
 							/></svg
 						>
 					</div>
-					<input type="text" placeholder="Amount" class="input px-2 py-1"   />
+					<input type="text" placeholder="Price" class="input px-2 py-1" />
 					<select>
 						<option>MKD</option>
 						<option>EUR</option>
@@ -33,15 +38,11 @@
 			<label class="label">
 				<span>Description</span>
 				<!-- (input here) -->
-				<input class="input px-2 py-1" title="description" type="text" placeholder="Description" />
+				<textarea class="textarea px-2 py-1" rows="4" placeholder="Description" />
 			</label>
-			<label class="label">
-				<span>Seller</span>
-				<!-- (input here) -->
-				<input class="input px-2 py-1" title="seller" type="text" placeholder="Seller" />
-			</label>
-			<FileDropzone name="files" class="mt-3 mb-3" />
-			<button type="button" class="btn variant-filled">Register</button>
+
+			<FileDropzone name="files" class="mb-3 mt-3" />
+			<button type="button" class="btn variant-filled">Register item</button>
 		</div>
 	</form>
 </div>
