56ed7dc853c3094a58391cb37e0b8a9512f7f2f7
StefanMrmov <stefanmrmov@yahoo.com>
2024-03-06 01:03:01 +0100

    Cool i Refined Dropzone
    very slick

--- oglas-mk/src/routes/add-item/+page.svelte
+++ oglas-mk/src/routes/add-item/+page.svelte
@@ -11,6 +11,12 @@
 		files.accepted = [...files.accepted, ...acceptedFiles];
 		files.rejected = [...files.rejected, ...fileRejections];
 	}
+
+	function handleRemoveFile(e, index) {
+		files.accepted.splice(index, 1);
+		files.accepted = [...files.accepted];
+	}
+
 </script>
 
 <!-- svelte-ignore a11y-label-has-associated-control -->
@@ -65,12 +71,30 @@
 				/>
 			</label>
 
-			<Dropzone on:drop={handleFilesSelect} />
-			<ol>
-				{#each files.accepted as item}
-					<li>{item.name}</li>
+			<Dropzone
+				on:drop={handleFilesSelect}
+				accept="image/jpeg, image/png, image/heif"
+				containerClasses="!rounded-lg !p-3 dark:!border-primary-600 dark:!bg-surface-600"
+			/>
+			<div style="margin: 5px;">
+				{#each files.accepted as item, index}
+					<div>
+						<span>{item.name}</span>
+						<button type="button" on:click={(e) => handleRemoveFile(e, index)}
+							><svg
+								xmlns="http://www.w3.org/2000/svg"
+								height="14"
+								viewBox="0 -960 960 960"
+								width="14"
+								class="fill-primary-600"
+								><path
+									d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
+								/></svg
+							></button
+						>
+					</div>
 				{/each}
-			</ol>
+			</div>
 			<button type="submit" class="btn variant-filled">Register item</button>
 		</div>
 	</form>
--- oglas-mk/src/routes/edit-item/[itemid]/+page.svelte
+++ oglas-mk/src/routes/edit-item/[itemid]/+page.svelte
@@ -6,9 +6,9 @@
 
 	//import { load } from './+page.server';
 
-      export async function loadPage({ params }) {
-        return ({ params });
-    }
+	export async function loadPage({ params }) {
+		return { params };
+	}
 
 	let files = {
 		accepted: [],
@@ -21,10 +21,14 @@
 		files.rejected = [...files.rejected, ...fileRejections];
 	}
 
+	function handleRemoveFile(e, index) {
+		files.accepted.splice(index, 1);
+		files.accepted = [...files.accepted];
+	}
 
-export let data;
-$: items = data.item;
-		</script>
+	export let data;
+	$: items = data.item;
+</script>
 
 <!-- svelte-ignore a11y-label-has-associated-control -->
 <div class="flex h-full w-full items-start justify-center px-4 pt-12">
@@ -34,7 +38,13 @@ $: items = data.item;
 			<label class="label" style="padding-top: 10px;">
 				<span>Title</span>
 				<!-- (input here) -->
-				<input class="input px-2 py-1" title="title" type="text" bind:value={items.title} placeholder="Title" />
+				<input
+					class="input px-2 py-1"
+					title="title"
+					type="text"
+					bind:value={items.title}
+					placeholder="Title"
+				/>
 			</label>
 			<label class="label">
 				<span>Price</span>
@@ -51,7 +61,7 @@ $: items = data.item;
 							/></svg
 						>
 					</div>
-					<input type="text"bind:value={items.price} placeholder="Price" class="input px-2 py-1" />
+					<input type="text" bind:value={items.price} placeholder="Price" class="input px-2 py-1" />
 					<select>
 						<option>MKD</option>
 						<option>EUR</option>
@@ -62,15 +72,38 @@ $: items = data.item;
 			<label class="label">
 				<span>Description</span>
 				<!-- (input here) -->
-				<textarea class="textarea px-2 py-1" rows="4"bind:value={items.description} placeholder="Description" />
+				<textarea
+					class="textarea px-2 py-1"
+					rows="4"
+					bind:value={items.description}
+					placeholder="Description"
+				/>
 			</label>
 
-			<Dropzone on:drop={handleFilesSelect} />
-			<ol>
-				{#each files.accepted as item}
-					<li>{item.name}</li>
+			<Dropzone
+				on:drop={handleFilesSelect}
+				accept="image/jpeg, image/png, image/heif"
+				containerClasses="!rounded-lg !p-3 dark:!border-primary-600 dark:!bg-surface-600"
+			/>
+			<div style="margin: 5px;">
+				{#each files.accepted as item, index}
+					<div>
+						<span>{item.name}</span>
+						<button type="button" on:click={(e) => handleRemoveFile(e, index)}
+							><svg
+								xmlns="http://www.w3.org/2000/svg"
+								height="14"
+								viewBox="0 -960 960 960"
+								width="14"
+								class="fill-primary-600"
+								><path
+									d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
+								/></svg
+							></button
+						>
+					</div>
 				{/each}
-			</ol>
+			</div>
 			<button type="button" class="btn variant-filled">Register item</button>
 		</div>
 	</form>
